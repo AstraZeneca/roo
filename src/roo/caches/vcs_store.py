a7b0da7a13b3df1503737c8c7998548ddd1ce95d
Stefano Borini <stefano.borini@astrazeneca.com>
2022-01-24 15:59:02 +0000

    Hardened the VCS store and the installation process

--- src/roo/caches/vcs_store.py
+++ src/roo/caches/vcs_store.py
@@ -12,46 +12,47 @@ logger = logging.getLogger(__file__)
 class VCSStore:
     """Store for the VCS clones we perform."""
 
-    def __init__(self, vcs_url: str, root_dir: Optional[pathlib.Path] = None):
-        self.vcs_url = vcs_url
+    def __init__(self, root_dir: Optional[pathlib.Path] = None):
         if root_dir is None:
             root_dir = pathlib.Path(tempfile.mkdtemp())
 
         self.root_dir = root_dir
 
-    @property
-    def base_dir(self) -> pathlib.Path:
+    def base_dir(self, vcs_url: str) -> pathlib.Path:
         """
-        Returns the base directory for the cache.
+        Returns the base directory for the cache for a given url
         """
         path = self.root_dir / "vcs"
         path.mkdir(parents=True, exist_ok=True)
-        url = urlparse(self.vcs_url)
+        url = urlparse(vcs_url)
         return self.root_dir / "vcs" / url.netloc / hashlib.sha256(
             url.path.encode("utf-8")).hexdigest()
 
-    def clone_dir(self, ref: Optional[str]) -> pathlib.Path:
+    def clone_dir(self, vcs_url: str, ref: Optional[str]) -> pathlib.Path:
         """Returns the directory where to clone the given ref"""
         if ref is None:
             ref = "HEAD"
-        return self.base_dir / ref
+        return self.base_dir(vcs_url) / ref
 
     def clear(self):
         """Clear the whole cache."""
-        logging.info(f"Clearing all vcs store at {self.base_dir}")
+        logging.info(f"Clearing all vcs stores at {self.root_dir}")
         try:
-            shutil.rmtree(self.base_dir)
+            shutil.rmtree(self.root_dir)
         except FileNotFoundError:
             pass
 
-    def clear_clone(self, ref: Optional[str]):
+    def clear_clone(self, vcs_url: str, ref: Optional[str]):
         """Clear the specified clone reference. Does nothing if not present"""
         if ref is None:
             ref = "HEAD"
 
-        logging.info(f"Clearing ref {ref}, vcs store at {self.base_dir}")
+        logging.info(
+            f"Clearing vcs store at {self.root_dir} "
+            f"for url {vcs_url} ref {ref}"
+        )
 
         try:
-            shutil.rmtree(self.clone_dir(ref))
+            shutil.rmtree(self.clone_dir(vcs_url, ref))
         except FileNotFoundError:
             pass
