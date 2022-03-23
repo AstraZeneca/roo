fcbee4de425515c4cb5dcf1eddab93975008c570
Stefano Borini <stefano.borini@astrazeneca.com>
2022-03-23 16:14:03 +0000

    Release 0.13.0

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2022, Stefano Borini'
 author = 'Stefano Borini'
 
 # The full version, including alpha/beta/rc tags
-release = '0.12.1'
+release = '0.13.0'
 
 
 # -- General configuration ---------------------------------------------------
