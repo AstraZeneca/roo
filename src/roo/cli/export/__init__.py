8478abde0b0030f20dabdb9bbb92d511aa741fca
Stefano Borini <stefano.borini@astrazeneca.com>
2022-07-27 15:33:39 +0100

    Added some messages for exporting

--- src/roo/cli/export/__init__.py
+++ src/roo/cli/export/__init__.py
@@ -1,5 +1,7 @@
 import click
+from roo.console import console
 from roo.cli.lock import _ensure_lock
+from roo.exporters.exceptions import ExportError
 from roo.exporters.lock.base_exporter import BaseExporter
 from roo.exporters.lock.lock_csv_exporter import LockCSVExporter
 from roo.exporters.lock.lock_packrat_exporter import LockPackratExporter
@@ -13,18 +15,31 @@ def export():
 
 @export.command(name="lock", help="Exports lock to different formats")
 @click.argument("format", type=click.Choice(["csv", "packrat", "renv"]))
-@click.argument("output", type=click.Path(writable=True))
+@click.argument("output", type=click.Path(writable=True), required=False)
 def export_lock(format, output):
     lock_file = _ensure_lock(False, False)
 
     exporter: BaseExporter
     if format == "csv":
         exporter = LockCSVExporter()
+        default_filename = "lock.csv"
     elif format == "packrat":
         exporter = LockPackratExporter()
+        default_filename = "packrat.lock"
     elif format == "renv":
         exporter = LockRenvExporter()
+        default_filename = "renv.lock"
     else:
         raise RuntimeError(f"Programming error. Not matched format {format}")
 
-    exporter.export(lock_file, output)
+    if output is None:
+        output = default_filename
+
+    try:
+        exporter.export(lock_file, output)
+    except ExportError as e:
+        console().print(f"[error]Unable to export lock file: {e}.[/error]")
+
+    console().print(
+        f"[success]Successfully exported lock file to {output}[/success]"
+    )
